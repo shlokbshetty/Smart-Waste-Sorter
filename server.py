8d5dd495d658f62d08fa68eeeb15d4cc3c1828e5
Shlok shetty <shettyshlok87@gmail.com>
2025-11-30 11:29:45 +0530

    Update server.py

--- server.py
+++ server.py
@@ -31,33 +31,21 @@ dry_classes = (
     'comic_book', 'menu', 'cardboard_box', 'carton', 'plastic_bag', 'garbage_truck', 'tin_can', 'milk_can',
     'beer_can', 'pill_bottle', 'packet', 'matchstick', 'nail', 'screw', 'ballpoint_pen', 'pencil_box',
     'paper_clip', 'rubber_eraser', 'wooden_spoon', 'plate', 'bowl', 'cup', 'beer_glass', 'wine_glass',
-    'coffee_mug', 'cellular_telephone', 'desktop_computer', 'notebook_computer', 'electric_fan', 'lamp',
-    'television', 'CD_player', 'iPod', 'digital_clock', 'analog_clock', 'wall_clock', 'hourglass',
-    'sunglasses', 'sunglass', 'binoculars', 'electric_guitar', 'acoustic_guitar', 'violin', 'cellular_telephone',
-    'mouse_trap', 'projectile', 'missile', '', 'candle', 'space_shuttle', 'nail', 'paintbrush', 'syringe',
-    'gasmask', 'diaper','airship', 'rubber_eraser', 'ballpoint', 'mailbag', 'toilet_tissue', 'switch',
-    'knot', 'file', 'necklace', 'notebook', 'perfume','purse', 'remote_control', 'radio', 'rugby_ball',
-    'scale', 'screw_driver', 'soap_dispenser', 'soup_bowl', 'spatula', 'swab', 'comic_book', 'wool',
-    'wooden_spoon', 'whistle', 'whiskey_jug', 'coral_reef', 'crossword_puzzle', 'street_sign', 'hot_pot',
-    'vase', 'tray', 'toaster', 'tennis_ball', 'snorkel', 'soccer_ball', 'sunscreen', 'safety_pin', 'saltshaker',
-    'scoreboard', 'screwdriver', 'shopping_basket', 'parachute', 'pencil_sharpener', 'Petri_dish', 'picket_fence',
-    'pinwheel', 'pirate', 'plane', 'planetarium', 'pole', 'modem', 'hook', 'lab_coat', 'measuring_cup', 'mask',
-    'magnetic_compass', 'lotion', 'lighter', 'letter_opener', 'lens_cap', 'lampshade', 'ladle', 'golf_ball', 'flagpole',
-    'handkerchief', 'coffeepot', 'Crock_Pot', 'croquet_ball', 'bottlecap', 'can_opener', 'carton', 'cleaver', 'bolo_tie',
-    'bath_towel', 'binder', 'packet', 'conch', 'binder', 'apron', 'monitor', 'lighter', 'pay-phone', 'tree_frog', 'nematode', 'tree_frog', 'American_alligator'
-)
+    'coffee_mug', 'Smartphone', 'iPod', 'analog_clock', 'wall_clock',
+    'sunglasses','candle', 'nail', 'paintbrush', 'diaper', 'rubber_eraser', 'toilet_tissue','file', 'necklace', 'notebook', 'perfume','purse', 'remote_control',
+    'scale', 'soup_bowl', 'comic_book',
+    'vase', 'tray', 'tennis_ball', 'safety_pin', 'pencil_sharpener', 'mask', 'lotion', 'lighter', 'lens_cap','golf_ball',
+    'handkerchief', 'coffeepot', 'Crock_Pot', 'bottlecap', 'can_opener','cleaver',
+    'bath_towel', 'packet', 'apron', 'lighter' )
 
 wet_classes = (
     'banana', 'apple', 'strawberry', 'orange', 'lemon', 'pineapple', 'jackfruit', 'custard_apple', 'pomegranate',
-    'fig', 'guava', 'mango', 'grapes', 'watermelon', 'cantaloupe', 'hone_dew', 'cucumber', 'eggplant',
+    'fig', 'guava', 'mango', 'grapes', 'watermelon', 'cucumber', 'eggplant',
     'bell_pepper', 'head_cabbage', 'broccoli', 'cauliflower', 'zucchini', 'spaghetti_squash', 'acorn_squash',
-    'butternut_squash', 'artichoke', 'cardoon', 'mushroom', 'Granny_Smith', 'earthstar', 'stinkhorn',
-    'hen-of-the-woods', 'bolete', 'corn', 'buckeye', 'organ', 'brain_coral', 'coral_fungus', 'agaric',
-    'gyromitra', 'face_powder', 'ice_cream', 'pizza', 'ping_pong_ball', 'ping-pong_ball', 'dough', 'meat_loaf',
-    'burrito', 'chocolate_sauce', 'eggnog', 'espresso', 'strawberry', 'butternut_squash', 'carbonara', 'potpie',
-    'red_wine', 'guacamole', 'consomme', 'trifle', 'ice_lolly', 'French_loaf', 'bagel', 'pretzel', 'cheeseburger',
-    'hotdog', 'mashed_potato', 'rotisserie', 'drumstick', 'snail', 'slug', 'jackfruit', 'buckeye'
-)
+    'butternut_squash', 'mushroom', 'corn', 'ice_cream', 'pizza', 'dough', 'meat_loaf',
+    'burrito', 'chocolate_sauce', 'red_wine', 'guacamole', 'consomme', 'trifle', 'ice_lolly', 
+    'French_loaf', 'bagel', 'cheeseburger', 'hotdog', 'mashed_potato', 'rotisserie', 'drumstick', 'buckeye'
+)    
 
 # --- Hardware Setup ---
 try:
@@ -147,11 +135,11 @@ def generate_frames():
                         
                         if name in wet_classes:
                             state["waste_type"] = "wet"
-                            state["prediction"] = f"{name} (Wet)"
+                            state["prediction"] = "Wet Waste" # Changed to generic text
                             found_waste = True
                         elif name in dry_classes:
                             state["waste_type"] = "dry"
-                            state["prediction"] = f"{name} (Dry)"
+                            state["prediction"] = "Dry Waste" # Changed to generic text
                             found_waste = True
                         
                         if found_waste:
